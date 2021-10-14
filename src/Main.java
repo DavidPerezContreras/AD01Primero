1d3c70a4e8d55fa4c2c0353efdaed33751563cd3
David Perez Contreras <david.perez.dam2@gmail.com>
2021-10-14 11:33:26 -0700

    AD01 Primero Terminado, o eso creo.

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,51 @@
 import java.io.File;
 
 public class Main {
-        static final File FHOME= new File("~");
+        static final File FHOME= new File(System.getProperty("user.home"));
 
     public static void main(String args[]){
 
     }
 
+
     public static void crear(){
         //Sólo se ejecuta en sistemas Linux
         if(System.getProperty("os.name").equals("Linux")){
-            (new File(FHOME,"Prueba")).mkdir();
+
+            new File(FHOME,"Pruebas/Primera/Tercera").mkdirs();
+            new File(FHOME,"Pruebas/Segunda").mkdirs();
         }else{
             System.out.println("Este método sólamente funciona en sistemas Windows");
         }
     }
 
-    public static void eliminar(){
 
+    //Elimina todos los directorios dentro de Pruebas, de abajo a arriba.
+    public static void eliminar(){
+        if(System.getProperty("os.name").equals("Linux")) {
+            new File(FHOME, "Pruebas/Primera/Tercera").delete();
+            new File(FHOME, "Pruebas/Primera/").delete();
+            new File(FHOME, "Pruebas/Segunda/").delete();
+        }else{
+            System.out.println("Este método sólamente funciona en sistemas Windows");
+        }
     }
 
-    public static void crear2(){
 
+
+
+    //Devuelve false si algun directorio no se ha podido crear,
+    //Sea cual sea el motivo (incluso si no se puede porque ya está creado).
+    public static boolean crear2(){
+        boolean exito=false;
+
+        if(System.getProperty("os.name").equals("Linux")) {
+             exito = new File(FHOME, "Pruebas/Primera/Tercera").mkdirs() & new File(FHOME, "Pruebas/Segunda").mkdirs();
+
+        }else{
+            System.out.println("Este método sólamente funciona en sistemas Windows");
+        }
+        return exito;
     }
 
 
